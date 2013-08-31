01389ccc6d38f3897b639940dc80747d786a095d
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2013-08-31 13:31:28 -0400

    Code to calculate the expiration of a task and track the next expiring task

--- wd_daemon.py
+++ wd_daemon.py
@@ -2,6 +2,7 @@
 
 import signal, sys, socket, select, watchdog_pb2
 import time
+import numpy
 
 RECV_BUFF_SIZE=4096
 
@@ -11,7 +12,17 @@ def sig_handler(signum, frame):
 def extract_sig(b):
     return (b.host + ":" + b.user + ":" + b.execed + ":" + str(b.pid))
 
+def expiration(t):
+    intervals = pwise_diff(t[-1000:])
+    mean = numpy.mean(intervals[-1000:])
+    std = numpy.std(intervals[-1000:])
+    e = (time.time() + (mean + 3.0 * std))
+    return e
+
 tasks = {}
+
+def pwise_diff(t):
+    return [t[i+1]-t[i] for i in range(len(t)-1)]
 
 def daemon(port):
     signal.signal(signal.SIGUSR1, sig_handler)
@@ -19,10 +30,30 @@ def daemon(port):
     inet.bind(('', port))
     inet.listen(1)
 
+    next_expiration(None, 3600)
+
     while True:
-        for x in select.select([inet],[],[],1)[0]: #Readable sockets returned by select
+        for x in select.select([inet],[],[],next_expiration[1])[0]: #Readable sockets returned by select
+            if x == None:
+                if next_expiration[0] != None:
+                    print(next_expiration[0] + " has expired.")
+                    for x in tasks:
+                else:
+                    continue
+
             c, client_addr = x.accept()
             data = c.recv(RECV_BUFF_SIZE)
             beat = watchdog_pb2.Heartbeat()
             beat.ParseFromString(data)
-            print(beat)
+            if beat.IsInitialized():
+                sig = extract_sig(beat)
+                try:
+                    t = tasks[sig]
+                    t.append(time.time())
+                    if len(t) > 1001:
+                        e = expiration(t)
+                        if e < next_expiration[1]:
+                            next_expiration = (sig, e)
+                except KeyError:
+                    tasks[sig] = []
+                    tasks[sig].append(time.time())
