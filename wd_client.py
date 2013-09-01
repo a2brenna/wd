4dd36d1b5893ac8a9349893afaada6719a5243f9
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2013-09-01 00:35:14 -0400

    More sensible defaults for arguments

--- wd_client.py
+++ wd_client.py
@@ -2,10 +2,10 @@
 
 import socket, watchdog_pb2
 
-def client(server, target_port):
+def client(server, target_port, command, delay):
     inet = socket.socket(socket.AF_INET)
     inet.connect((server, target_port))
     beat = watchdog_pb2.Heartbeat()
-    beat.signature = 'test_user:host:test_application:42'
+    beat.signature = 'test_user:demogorgon:test_application:42'
     inet.send(beat.SerializeToString())
     inet.close()
