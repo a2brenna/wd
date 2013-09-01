468fbae119557a22ab94f8e182e47198e944c274
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2013-08-31 20:02:16 -0400

    Graceful exit and signal handling

--- wd_daemon.py
+++ wd_daemon.py
@@ -6,9 +6,15 @@ import numpy
 
 RECV_BUFF_SIZE=4096
 
+def exit():
+    sys.exit(0)
+
 def sig_handler(signum, frame):
-    import pprint
-    pprint.pprint(tasks)
+    if signum == signal.SIGUSR1:
+        import pprint
+        pprint.pprint(tasks)
+    else:
+        exit()
 
 def extract_sig(b):
     return (b.host + ":" + b.user + ":" + b.execed + ":" + str(b.pid))
@@ -34,34 +40,40 @@ def daemon(port):
     next_expiration = (None, 3600)
 
     while True:
-        for x in select.select([inet],[],[],next_expiration[1])[0]: #Readable sockets returned by select
-            if x == None:
-                if next_expiration[0] != None:
-                    print(next_expiration[0] + " has expired.")
-                    del tasks[next_expiration[0]]
+        try:
+            for x in select.select([inet],[],[],next_expiration[1])[0]: #Readable sockets returned by select
+                if x == None:
+                    if next_expiration[0] != None:
+                        print(next_expiration[0] + " has expired.")
+                        del tasks[next_expiration[0]]
 
-                    min_expiration = (None, 3600)
-                    for x in tasks.keys():
-                        if tasks[x] < min_expiration[1]:
-                            min_expiration = (x, tasks[x])
+                        min_expiration = (None, 3600)
+                        for x in tasks.keys():
+                            if tasks[x] < min_expiration[1]:
+                                min_expiration = (x, tasks[x])
 
-                    next_expiration = min_expiration
-                else:
-                    continue
+                        next_expiration = min_expiration
+                    else:
+                        continue
 
-            c, client_addr = x.accept()
-            data = c.recv(RECV_BUFF_SIZE)
-            beat = watchdog_pb2.Heartbeat()
-            beat.ParseFromString(data)
-            if beat.IsInitialized():
-                sig = extract_sig(beat)
-                try:
-                    t = tasks[sig]
-                    t.append(time.time())
-                    if len(t) > 1001:
-                        e = expiration(t)
-                        if e < next_expiration[1]:
-                            next_expiration = (sig, e)
-                except KeyError:
-                    tasks[sig] = []
-                    tasks[sig].append(time.time())
+                c, client_addr = x.accept()
+                data = c.recv(RECV_BUFF_SIZE)
+                beat = watchdog_pb2.Heartbeat()
+                beat.ParseFromString(data)
+                if beat.IsInitialized():
+                    sig = extract_sig(beat)
+                    try:
+                        t = tasks[sig]
+                        t.append(time.time())
+                        if len(t) > 1001:
+                            e = expiration(t)
+                            if e < next_expiration[1]:
+                                next_expiration = (sig, e)
+                    except KeyError:
+                        tasks[sig] = []
+                        tasks[sig].append(time.time())
+        except KeyboardInterrupt:
+            exit()
+        except:
+            continue
+            
