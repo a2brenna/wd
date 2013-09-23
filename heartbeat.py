6dbd3ee1f7cc37b129b4e470535387543ee12b5d
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2013-09-23 01:52:51 -0400

    Move implementation of query to heartbeat.py

--- heartbeat.py
+++ heartbeat.py
@@ -12,3 +12,18 @@ def beat(server, port, signature=gen_sig()):
     s.connect((server,port))
     s.send(message.SerializeToString())
     s.close
+
+def query(server, port):
+    message = watchdog_pb2.Message()
+    message.query.question = "Status"
+
+    s = socket.socket(socket.AF_INET)
+    s.connect((server,port))
+    s.send(message.SerializeToString())
+
+    response = watchdog_pb2.Message()
+    data = s.recv(4096)
+    s.close
+
+    response.ParseFromString(data)
+    return response.response
