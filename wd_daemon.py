7483a7ab5b5accb6616733e2a7cc8f2467c1982a
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2013-09-25 23:17:18 -0400

    Ensure that we never have negative itimer sleep values, which might be possible if a heartbeat expires while we are handling a signal

--- wd_daemon.py
+++ wd_daemon.py
@@ -95,7 +95,7 @@ def awake(signum, frame):
         logging.info("Next expiration is " + next_expiration.signature + " @ " + str(next_expiration.expiration))
     except:
         next_expiration = None
-        signal.setitimer(signal.ITIMER_REAL, beat_time + 60.0 - time.time())
+        signal.setitimer(signal.ITIMER_REAL, max(beat_time + 60.0 - time.time(), 0.001))
         logging.info("No pending expiration")
 
 def daemon(port, dumpdir, wd_server, wd_port):
