df8773df22212f8c987c183f2cee620b1dd4b80c
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2013-12-08 00:26:29 -0500

    Simple setup.py script for using distutils

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='wd',
+      version='1.0',
+      author='Anthony Brennan',
+      author_email='a2brenna@csclub.uwaterloo.ca',
+      url='http://csclub.uwaterloo.ca/~a2brenna/wd.html',
+      py_modules=['wd'],
+      )
