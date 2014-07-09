1ff5346ca8d0d61540f4b29149235e15ca96194f
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2014-07-09 12:21:07 -0400

    Write basic beat() functionaity

--- src/pitbull.h
+++ src/pitbull.h
@@ -1,6 +1,11 @@
 #ifndef __PITBULL_H__
 
 #include <hgutil/server.h>
+#include <string>
+#include <map>
+#include <deque>
+
+const int max_ivals = 10000;
 
 class Incoming_Connection : public Task{
     public:
@@ -8,7 +13,20 @@ class Incoming_Connection : public Task{
         Incoming_Connection(int s) { sockfd = s; };
 };
 
+class Task_Data {
+    private:
+        std::deque<double> ivals;
+    public:
+        double last = -1;
+        double mean = 0;
+        double deviation = 0;
+
+        void beat();
+};
+
 class Pitbull : public Handler{
+    private:
+        std::map<std::string, Lockable<Task_Data>> tracked_tasks;
     public:
         void handle(Task *t);
 };
