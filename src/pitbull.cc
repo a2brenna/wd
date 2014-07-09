b4413e6254b6592332e7045d0ac0671777ca8231
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2014-07-09 12:21:07 -0400

    Write basic beat() functionaity

--- src/pitbull.cc
+++ src/pitbull.cc
@@ -1,5 +1,32 @@
 #include "pitbull.h"
+#include <hgutil/time.h>
+
+void Task_Data::beat(){
+    int s = ivals.size();
+    while( s > (max_ivals - 1) ){
+        ivals.pop_back();
+    }
+
+    auto current = milli_time() / 1000.0;
+
+    if (last != -1 ){
+        ivals.push_front(current - last);
+    }
+
+    last = current;
+
+    //calculate expiration
+    //
+    ///update itimer for expiration wakeup
+
+    return;
+}
 
 void Pitbull::handle(Task *t){
+    if(Incoming_Connection *i = dynamic_cast<Incoming_Connection *>(t)){
 
+    }
+    else{
+        throw Handler_Exception("Bad task");
+    }
 }
