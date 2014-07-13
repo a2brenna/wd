aa1ffcb927a1f69bbfd4198cd82258c8f7561169
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2014-07-13 16:43:54 -0400

    Wrap beat handling in a loop that exits upon connection error

--- src/pitbull.cc
+++ src/pitbull.cc
@@ -2,7 +2,9 @@
 #include "watchdog.pb.h"
 #include <mutex>
 #include <hgutil/time.h>
-#include <hgutil/network.h>
+#include <hgutil/socket.h>
+#include <hgutil/fd.h>
+#include <iostream>
 
 void Task_Data::beat(){
     int s = ivals.size();
@@ -26,34 +28,43 @@ void Task_Data::beat(){
 }
 
 void Pitbull::handle(Task *t){
+    std::cerr << "Got incoming client" << std::endl;
     if(Incoming_Connection *i = dynamic_cast<Incoming_Connection *>(t)){
-        std::string request;
-        //TODO: can't just recv_string off plain socket... TLS magic needs to happen first
-        //recv_string(i->sockfd, request);
+        for (;;){
+            try{
+                std::string request;
+                recv_string(i->sock, request);
 
-        watchdog::Message m;
-        m.ParseFromString(request);
+                watchdog::Message m;
+                m.ParseFromString(request);
 
-        if(m.has_beat()){
-            Lockable<Task_Data> *task;
-            {
-                std::lock_guard<std::recursive_mutex> t(tracked_tasks.lock);
-                task = &(tracked_tasks.data[m.beat().signature()]);
+                if(m.has_beat()){
+                    std::cerr << "Parsed incoming beat" << std::endl;
+                    Lockable<Task_Data> *task;
+                    {
+                        std::lock_guard<std::recursive_mutex> t(tracked_tasks.lock);
+                        task = &(tracked_tasks.data[m.beat().signature()]);
+                    }
+
+                    std::lock_guard<std::recursive_mutex> t(task->lock);
+                    task->data.beat();
+                    std::cout << "Task: " << m.beat().signature() << " has " << task->data.num_beats() << std::endl;
+                }
+                else if(m.has_query()){
+
+                }
+                else if(m.orders_size() > 0){
+
+                }
+                else{
+                    throw Handler_Exception("Bad Request");
+                }
             }
-
-            std::lock_guard<std::recursive_mutex> t(task->lock);
-            task->data.beat();
+            catch(Network_Error e){
+                std::cerr << "Network_Error: " << e.msg << std::endl;
+                break;
+            }
         }
-        else if(m.has_query()){
-
-        }
-        else if(m.orders_size() > 0){
-
-        }
-        else{
-            throw Handler_Exception("Bad Request");
-        }
-
     }
     else{
         throw Handler_Exception("Bad task");
