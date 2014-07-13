71d0076309d30427351b0972e5c7bdbd99a76956
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2014-07-13 16:44:41 -0400

    Use Secure_Socket() over Unix Domain

--- src/pb.cc
+++ src/pb.cc
@@ -1,22 +1,37 @@
 #include "pitbull.h"
-#include <hgutil/network.h>
+#include <hgutil/socket.h>
+#include <hgutil/fd.h>
 #include <memory>
+#include <gnutls/gnutls.h>
+#include <iostream>
 
 const int PORT = 7877;
-const int HEARTRATE = 60;
-const int MAX_ATTEMPTS = 5;
+
+auto CERTFILE = "/home/a2brenna/.ssl/cert.pem";
+auto KEYFILE = "/home/a2brenna/.ssl/key.pem";
+auto CAFILE = "/home/a2brenna/.ssl/ca-cert.pem";
 
 int main(){
 
+    gnutls_certificate_credentials_t x509_cred = tls_init(KEYFILE, CERTFILE, CAFILE);
+
     Connection_Factory ears{};
-    ears.add_socket( listen_on(PORT, false) );
+    int port1 = listen_on("/tmp/pb.sock", false);
+    ears.add_socket(port1);
 
     Pitbull p{};
 
     for(;;){
-        std::shared_ptr<Task> t(new Incoming_Connection(ears.next_connection()));
-        p.queue_task(t);
-        p.handle_next();
+        try{
+            int next = ears.next_connection();
+            std::cerr << "Got incoming" << std::endl;
+            std::shared_ptr<Task> t(new Incoming_Connection(new Secure_Socket(next, true, x509_cred)));
+            p.queue_task(t);
+            p.handle_next();
+        }
+        catch(Network_Error e){
+            std::cerr << "Network_Error: " << e.msg << std::endl;
+        }
     }
 
     return 0;
