8140ee21518f7ff0c7b742337358ce4444f97f1e
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2014-07-14 13:02:40 -0400

    Check for message initialization

--- src/pitbull.cc
+++ src/pitbull.cc
@@ -7,6 +7,47 @@
 #include <hgutil/math.h>
 #include <iostream>
 #include <chrono>
+#include <syslog.h>
+
+void Pitbull::handle(Task *t){
+    if(Incoming_Connection *i = dynamic_cast<Incoming_Connection *>(t)){
+        for (;;){
+            try{
+                std::string request;
+                recv_string(i->sock, request);
+
+                watchdog::Message m;
+                m.ParseFromString(request);
+
+                if(m.IsInitialized()){
+                    if(m.has_beat()){
+                        handle_beat(m);
+                    }
+                    else if(m.has_query()){
+
+                    }
+                    else if(m.orders_size() > 0){
+
+                    }
+                    else{
+                        throw Handler_Exception("Bad Request");
+                    }
+                }
+                else{
+                    syslog(LOG_ERR, "Uninitialized Message: %s", m.DebugString().c_str());
+                    break;
+                }
+            }
+            catch(Network_Error e){
+                std::cerr << "Network_Error: " << e.msg << std::endl;
+                break;
+            }
+        }
+    }
+    else{
+        throw Handler_Exception("Bad task");
+    }
+}
 
 void Pitbull::reset_expiration(){
 
@@ -45,38 +86,4 @@ void Pitbull::handle_beat(watchdog::Message m){
 
     std::lock_guard<std::recursive_mutex> time_lock(timelock);
     reset_expiration();
-}
-
-void Pitbull::handle(Task *t){
-    if(Incoming_Connection *i = dynamic_cast<Incoming_Connection *>(t)){
-        for (;;){
-            try{
-                std::string request;
-                recv_string(i->sock, request);
-
-                watchdog::Message m;
-                m.ParseFromString(request);
-
-                if(m.has_beat()){
-                    handle_beat(m);
-                }
-                else if(m.has_query()){
-
-                }
-                else if(m.orders_size() > 0){
-
-                }
-                else{
-                    throw Handler_Exception("Bad Request");
-                }
-            }
-            catch(Network_Error e){
-                std::cerr << "Network_Error: " << e.msg << std::endl;
-                break;
-            }
-        }
-    }
-    else{
-        throw Handler_Exception("Bad task");
-    }
 }
