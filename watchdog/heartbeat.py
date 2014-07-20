22475ff3310a184299a03613e9a7923d353718e8
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2014-07-19 20:10:00 -0400

    Fix up some type problems

--- watchdog/heartbeat.py
+++ watchdog/heartbeat.py
@@ -2,7 +2,8 @@ import watchdog_pb2, socket, os, psutil, logging, ssl, psutil
 
 def gen_sig(pid=os.getpid()):
     p = psutil.Process(pid)
-    return (p.username + ":" + socket.gethostname() + ":" + p.name + ":" + str(pid))
+
+    return (str(p.username) + ":" + str(socket.gethostname()) + ":" + str(p.name) + ":" + str(pid))
 
 def beat(server, port, signature=gen_sig()):
     logging.info("Sending beat with signature " + signature)
