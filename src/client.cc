6d4c76c30916cb97851754aa5468b16e8e80cf5d
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-05-03 07:54:33 -0400

    Add unconfigured Heart

--- src/client.cc
+++ src/client.cc
@@ -2,11 +2,13 @@
 #include "watchdog.pb.h"
 
 void Heart::beat(){
-    watchdog::Message m;
-    m.mutable_beat()->set_signature(_id);
+    if( (_server != nullptr) && (_id != "") ){
+        watchdog::Message m;
+        m.mutable_beat()->set_signature(_id);
 
-    std::string message;
-    m.SerializeToString(&message);
+        std::string message;
+        m.SerializeToString(&message);
 
-    _server->send(message);
+        _server->send(message);
+    }
 }
