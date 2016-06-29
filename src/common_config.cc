d4ea1aafecbf97fe4e456f09363ae8437189752f
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2016-06-29 23:09:52 +0000

    Fix misnamed config file

--- src/common_config.cc
+++ src/common_config.cc
@@ -1,5 +1,5 @@
 #include "common_config.h"
 
-std::string global_config_file = "/etc/wd.conf";
+std::string global_config_file = "/etc/watchdog/wd.conf";
 std::string CONFIG_SERVER_ADDRESS = "happiestface.convextech.ca";
 int CONFIG_INSECURE_PORT = 7876;
